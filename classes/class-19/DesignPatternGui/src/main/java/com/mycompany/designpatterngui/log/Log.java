08f512152ab5e003d0cc47998d190dee22d2d4f0
pabloMarquez88 <pablo.marquez88@gmail.com>
2022-09-30 08:43:32 -0300

    Added design patter class

---
+++ classes/class-19/DesignPatternGui/src/main/java/com/mycompany/designpatterngui/log/Log.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.designpatterngui.log;
+
+/**
+ *
+ * @author Escritorio
+ */
+public final class Log implements LoggerInterface{
+    
+    @Override
+    public void log(Object data){
+        
+    }
+    
+}
---
+++ classes/class-19/DesignPatternGui/src/main/java/com/mycompany/designpatterngui/log/LoggerInterface.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.designpatterngui.log;
+
+/**
+ *
+ * @author Escritorio
+ */
+public interface LoggerInterface {
+     public void log(Object data);
+}
